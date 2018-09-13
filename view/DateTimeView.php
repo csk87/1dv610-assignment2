7a31c1b7154da60d2e6b7c7ba12618299353068c
ck222re <csk87@live.se>
2018-09-13 12:26:35 +0200

    Added working DateTimeView.php (the date and time is displayed correct)

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -2,10 +2,17 @@
 
 class DateTimeView {
 
-
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$today = getdate();
+
+		$dayOfWeek = $today['weekday'];
+		$dayOfMonthNumber = $today['mday'];
+		$monthAsText = $today['month'];
+		$yearInDigits = $today['year'];
+		$currentTime = date('h:i:s'); //using date() insted of getdate() to get leading zero
+		
+		$timeString = "{$dayOfWeek}, the {$dayOfMonthNumber}th of {$monthAsText} {$yearInDigits}, The time is {$currentTime}";
 
 		return '<p>' . $timeString . '</p>';
 	}
