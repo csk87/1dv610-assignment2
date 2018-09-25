0e4de3d96450697f442b786835aa1d7d5fc1bd58
ck222re <csk87@live.se>
2018-09-25 16:00:24 +0200

    some method refactoring in LoginController

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -22,39 +22,42 @@ class LoginController{
 
   public function response(){
     
-    // om man redan är inloggad (session redan satt)
     if($this->loginView->sessionExist()){
       $this->isLoggedIn = true;
-
-      //om man klickar på logga ut 
-      if($this->loginView->tryToLogout()){ // logout knappen är klickad
-        $this->message = 'Bye bye!';
-        $this->isLoggedIn = false; 
-        $this->loginView->removeCurrentSession();
-      }
-     
+      $this->handleLogutRequest();
     }
 
-    // om vi försöker logga in och det inte finns någon session satt. 
     else if($this->loginView->tryToLogin()){
-
-      //validerar input fälten
-      if($this->loginView->getUsernameInput() == ''){
-        $this->message = 'Username is missing';
-      } else if($this->loginView->getPasswordInput() == ''){
-        $this->message = 'Password is missing';
-      } else if($this->loginView->getUsernameInput() !== $this->userModel->getUsername() || $this->loginView->getPasswordInput() !== $this->userModel->getPassword()){
-        $this->message = 'Wrong name or password';
-      }
-
-      //om alla fälten var korret ifyllda
-      else {
-        $this->loginView->setSessionName();
-        $this->message = 'Welcome';
-        $this->isLoggedIn = true;
-      } 
+      $this->handleLoginRequest();
     }
     $this->layoutView->render($this->isLoggedIn, $this->message, $this->loginView, $this->dateTimeView);  
+  }
+
+
+  private function handleLogutRequest(){
+    if(!$this->loginView->tryToLogout()){ 
+      return;
+    }
+    $this->message = 'Bye bye!';
+    $this->isLoggedIn = false; 
+    $this->loginView->removeCurrentSession();
+  }
+
+  private function handleLoginRequest(){
+    //validerar input fälten
+    if($this->loginView->getUsernameInput() == ''){
+      $this->message = 'Username is missing';
+    } else if($this->loginView->getPasswordInput() == ''){
+      $this->message = 'Password is missing';
+    } else if($this->loginView->getUsernameInput() !== $this->userModel->getUsername() || $this->loginView->getPasswordInput() !== $this->userModel->getPassword()){
+      $this->message = 'Wrong name or password';
+    }
+    //om alla fälten var korret ifyllda
+    else {
+      $this->loginView->setSessionName();
+      $this->message = 'Welcome';
+      $this->isLoggedIn = true;
+    } 
   }
 
 }
